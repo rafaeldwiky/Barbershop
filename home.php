296640d6c4608dccc3f8b49a49d8ba9292d26184
Fahmi Kurniawan <pahmilucu123@gmail.com>
2023-11-06 20:07:14 +0700

    menghapus beberapa div yang tidak diperlukan di halaman dashboard

--- home.php
+++ home.php
@@ -491,13 +491,13 @@ if (isset($_SESSION['msg'])) {
                     </div>
 
                     <!-- Content Row -->
-                    <div class="row">
+                    <!-- <div class="row"> -->
 
-                        <!-- Content Column -->
-                        <div class="col-lg-6 mb-4">
+                    <!-- Content Column -->
+                    <!-- <div class="col-lg-6 mb-4"> -->
 
-                            <!-- Project Card Example -->
-                            <div class="card shadow mb-4">
+                    <!-- Project Card Example -->
+                    <!-- <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
                                 </div>
@@ -533,10 +533,10 @@ if (isset($_SESSION['msg'])) {
                                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
 
-                            <!-- Color System -->
-                            <div class="row">
+                    <!-- Color System -->
+                    <!-- <div class="row">
                                 <div class="col-lg-6 mb-4">
                                     <div class="card bg-primary text-white shadow">
                                         <div class="card-body">
@@ -605,10 +605,10 @@ if (isset($_SESSION['msg'])) {
 
                         </div>
 
-                        <div class="col-lg-6 mb-4">
+                        <div class="col-lg-6 mb-4"> -->
 
-                            <!-- Illustrations -->
-                            <div class="card shadow mb-4">
+                    <!-- Illustrations -->
+                    <!-- <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
                                 </div>
@@ -624,10 +624,10 @@ if (isset($_SESSION['msg'])) {
                                     <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
                                         unDraw &rarr;</a>
                                 </div>
-                            </div>
+                            </div> -->
 
-                            <!-- Approach -->
-                            <div class="card shadow mb-4">
+                    <!-- Approach -->
+                    <!-- <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
                                 </div>
@@ -641,7 +641,7 @@ if (isset($_SESSION['msg'])) {
                             </div>
 
                         </div>
-                    </div>
+                    </div> -->
 
                 </div>
                 <!-- /.container-fluid -->
@@ -664,7 +664,7 @@ if (isset($_SESSION['msg'])) {
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <!-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -681,7 +681,7 @@ if (isset($_SESSION['msg'])) {
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
